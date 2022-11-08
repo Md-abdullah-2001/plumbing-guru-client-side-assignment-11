74b356c0b95a19059a61577441c94ead8f7eb65e
Md-abdullah-2001 <mdabdullah.dm2001@gmail.com>
2022-11-08 15:46:10 +0600

    added navbar and responsiveness

--- src/Shared/NavBar/NavBar.js
+++ src/Shared/NavBar/NavBar.js
@@ -1,9 +1,52 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import logo from "../../Asset/favicon.svg";
 const NavBar = () => {
+  const menuList = (
+    <>
+      <li className="mr-8 font-bold text-green-700">Home</li>
+      <li className="mr-8 font-bold text-green-700">Services</li>
+      <li className="mr-8 font-bold text-green-700">Blogs</li>
+    </>
+  );
   return (
-    <div>
-      <h1>Navbar goes here</h1>
+    <div className="navbar bg-fuchsia-500">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {menuList}
+          </ul>
+        </div>
+        <img src={logo} alt="" srcset="" />
+        <Link to="/" className="btn btn-ghost normal-case text-xl">
+          Plumbing-Guru
+        </Link>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal p-0">{menuList}</ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn">Get started</a>
+      </div>
     </div>
   );
 };
