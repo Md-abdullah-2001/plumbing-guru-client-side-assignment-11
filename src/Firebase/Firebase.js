da5dfd1def59301a2bf1a88fc816b6aa85d714dd
Md-abdullah-2001 <mdabdullah.dm2001@gmail.com>
2022-11-10 10:03:41 +0600

    environment file added

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAzvPLe5BxJLEZhRJyvHs2_E1FBajRuRjU",
-  authDomain: "plumbing-review.firebaseapp.com",
-  projectId: "plumbing-review",
-  storageBucket: "plumbing-review.appspot.com",
-  messagingSenderId: "306936521997",
-  appId: "1:306936521997:web:0e0c04cd38ad662a393c44",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
